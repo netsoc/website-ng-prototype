13c4800125b7bf3c1cfce807c8a1104ce66e1ffe
dyllew3 <lewisdy@tcd.ie>
2019-05-20 16:15:23 +0100

    Setup the mariadb in docker and setup a basic table for the blog post no data added yet. Also requirements updated

--- app/netsoc.py
+++ app/netsoc.py
@@ -1,13 +1,65 @@
 #!/usr/bin/env python
 from os import environ
 
+
 from werkzeug.middleware.proxy_fix import ProxyFix
 from flask import Flask, render_template
+from sqlalchemy.engine.url import URL
+from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 # Make sure request.remote_addr represents the real client IP
 #app.wsgi_app = ProxyFix(app.wsgi_app)
+
+maria_db = {
+    'drivername': 'mysql+mysqlconnector',
+    'username': environ['MYSQL_USER'], 
+    'password': environ['MYSQL_PASSWORD'],
+    # This is specific to this setup will need to change 
+    'host': 'db',
+    'port': 3306,
+    'database': environ['MYSQL_DATABASE']
+
+}
+
+app.config["SQLALCHEMY_DATABASE_URI"] = URL(**maria_db)
+db = SQLAlchemy(app)
+
+
+# Had to include these due to how database was made(legacy issues wooo)
+from sqlalchemy.dialects.mysql import INTEGER, BIGINT, LONGTEXT, MEDIUMTEXT, VARCHAR
+
+# Class used for blog posts on the netsoc website
+class BlogPost(db.Model):
+    id = db.Column(BIGINT(unsigned=True), primary_key=True)
+    post_author    = db.Column(BIGINT(unsigned=True), unique=False, nullable=False)
+    post_date      = db.Column(db.DateTime,unique=False, nullable=False)
+    post_date_gmt  = db.Column(db.DateTime,unique=False, nullable=False)
+    post_content   = db.Column(LONGTEXT, unique=False, nullable=False)
+    post_title     = db.Column(MEDIUMTEXT,nullable=False)
+    post_category  = db.Column(db.Integer)
+    post_excerpt   = db.Column(MEDIUMTEXT)
+    post_status    = db.Column(VARCHAR(20))
+    comment_status = db.Column(VARCHAR(20))
+    ping_status    = db.Column(VARCHAR(20))
+    post_password  = db.Column(VARCHAR(255))
+    post_name      = db.Column(VARCHAR(200), unique=False)
+    to_ping        = db.Column(MEDIUMTEXT)
+    pinged         = db.Column(MEDIUMTEXT)
+    post_modified  = db.Column(db.DateTime)
+    post_modified_gmt = db.Column(db.DateTime)
+    post_content_filtered = db.Column(LONGTEXT)
+    post_parent    = db.Column(BIGINT(unsigned=True))
+    guid           = db.Column(VARCHAR(255))
+    menu_order     = db.Column(db.Integer)
+    post_type      = db.Column(VARCHAR(20))
+    post_mime_type = db.Column(VARCHAR(100))
+    comment_count  = db.Column(BIGINT)
+
+# Create tables defined above if they don't exist otherwise load them in
+db.create_all()
 
 # Configuration is provided through environment variables by Docker Compose
 development = not environ['FLASK_ENV'] == 'production'
@@ -21,6 +73,7 @@ app.config.update({
 
 @app.route('/')
 def home():
+    print(db.metadata.tables)
     return render_template("home.html")
 
 @app.route('/about-us')
