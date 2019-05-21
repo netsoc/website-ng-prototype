64db9fa051fa73de026b7eea3e32bc29f923b164
Jack O'Sullivan <jackos1998@gmail.com>
2019-05-21 17:57:17 +0100

    Fix CLI import command argument parsing

--- app/cli/__init__.py
+++ app/cli/__init__.py
@@ -16,12 +16,12 @@ def run():
     p_app = subparsers.add_parser('app', help='Run the development server')
     p_app.set_defaults(func=c_dev)
 
-    p_app = subparsers.add_parser('import', help='Import blog posts from WordPress')
-    p_app.add_argument('address', help='Address of the MySQL instance hosting WordPress')
-    p_app.add_argument('-p', '--port', help='MySQL port', type=int, default=3306)
-    p_app.add_argument('database', help='WordPress database name')
-    p_app.add_argument('user', help='WordPress database user')
-    p_app.set_defaults(func=wp_import.run)
+    p_import = subparsers.add_parser('import', help='Import blog posts from WordPress')
+    p_import.add_argument('address', help='Address of the MySQL instance hosting WordPress')
+    p_import.add_argument('-p', '--port', help='MySQL port', type=int, default=3306)
+    p_import.add_argument('database', help='WordPress database name')
+    p_import.add_argument('user', help='WordPress database user')
+    p_import.set_defaults(func=wp_import.run)
 
     args = parser.parse_args()
     args.func(args)
